b2344c84e50213303ea187193df64f4c980e5cc1
Austin Miller <admin@example.com>
2021-05-13 20:50:20 +0000

    Added print grid functionality

---
+++ solver.py
@@ -0,0 +1,20 @@
+# Size of the grid
+grid_size = 9
+
+# 0 means unassigned cells
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+def print_grid(arr):
+    for row in range(grid_size):
+        for col in range(grid_size):
+            print(str(arr[row][col]), end = ' ')
+        print()
+
