3e5a2c2a352f92135542fbf085eb4fd2c6b87680
Austin Miller <austin@millerfamily.cc>
2021-05-13 20:16:53 -0500

    Fixed check exists in row and col

--- solver.py
+++ solver.py
@@ -20,11 +20,11 @@ def print_grid(arr):
 
 def check_exists_in_row(arr, row, num):
     if num in arr[row]:
-        return true
-    return false
+        return True
+    return False
 
 def check_exists_in_col(arr, col, num):
     for row in arr:
         if row[col] == num:
-            return true
-    return false
+            return True
+    return False
