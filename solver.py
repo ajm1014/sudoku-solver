db35037aaf662e2df275ab9e9f775d2e4c890237
Austin Miller <austin@millerfamily.cc>
2021-05-13 22:03:29 -0500

    Finished backtracking method and helper functions

--- solver.py
+++ solver.py
@@ -12,29 +12,59 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 7, 4],
         [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
+
 def print_grid(arr):
     for row in range(grid_size):
         for col in range(grid_size):
             print(str(arr[row][col]), end = ' ')
         print()
 
-def check_exists_in_row(arr, row, num):
+
+def is_safe(arr, row, col, num):
     if num in arr[row]:
-        return True
-    return False
+        return False
 
-def check_exists_in_col(arr, col, num):
-    for row in arr:
-        if row[col] == num:
-            return True
-    return False
+    for single_row in arr:
+        if single_row[col] == num:
+            return False
 
-def check_exists_in_square(arr, row, col, num):
     top_left = [(int(row / 3) * 3), (int(col / 3) * 3)]
     for row_num in range(top_left[0], (top_left[0] + 3)):
         for col_num in range(top_left[1], (top_left[1] + 3)):
             if arr[row_num][col_num] == num:
+                return False
+
+    return True
+
+def get_next_blank(arr, cell):
+    for row_num in range(grid_size):
+        for col_num in range(grid_size):
+            if arr[row_num][col_num] == 0:
+                cell[0] = row_num
+                cell[1] = col_num
                 return True
     return False
 
-print(check_exists_in_square(grid, 2, 5, 3))
+def solve_puzzle(arr):
+    cell = [0, 0]
+
+    if not get_next_blank(arr, cell):
+        return True
+
+    for num in range(1, 1 + grid_size):
+        if is_safe(arr, cell[0], cell[1], num):
+            arr[cell[0]][cell[1]] = num
+
+            if solve_puzzle(arr):
+                return True
+            
+            arr[cell[0]][cell[1]] = 0
+
+    return False
+
+
+
+if solve_puzzle(grid):
+    print_grid(grid)
+else:
+    print("This puzzle cannot be solved.")
